731dd6f5671c588ed7a9cee59630aec1ab103ad1
CypherPoet <CypherPoet@gmail.com>
2019-01-15 01:28:38 -0500

    Finish Project 03

---
+++ 03-social-media/Storm Viewer/DetailViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Brian Sipple on 1/12/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var imagePath: String?
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        get {
+            return navigationController?.hidesBarsOnTap ?? false
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = imagePath
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(shareTapped)
+        )
+        
+        if let _imagePath = imagePath {
+            imageView.image = UIImage(named: _imagePath)
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    @objc func shareTapped() {
+        let controller = UIActivityViewController(
+            activityItems: [imageView.image!],
+            applicationActivities: []
+        )
+        
+        // tell iOS where the controller should be anchored
+        controller.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(controller, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
