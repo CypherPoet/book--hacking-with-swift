9671f8cee22ee9b3eebbaaa9c928a6aaa8358d30
CypherPoet <CypherPoet@gmail.com>
2019-01-16 00:17:01 -0500

    Initial setup for World Flag Sharing challenge

---
+++ challenges/world-flag-sharing/World Flag Sharing/World Flag Sharing/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  World Flag Sharing
+//
+//  Created by Brian Sipple on 1/15/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- challenges/world-flag-sharing/World Flag Sharing/World Flag Sharing/ViewController.swift
+++ challenges/world-flag-sharing/World Flag Sharing/World Flag Sharing/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var flagImagePaths: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let fileManager = FileManager.default
+        let flagImagesPath = "(Bundle.main.resourcePath!)/flags"
+        
+        flagImagePaths = try! fileManager.contentsOfDirectory(atPath: flagImagesPath)
     }
-
-
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return flagImagePaths.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // load the detail view controller on to the navigation controller stack
+        if let detailVC = storyboard?.instantiateViewController(withIdentifier: "Flag Detail") as? DetailViewController {
+            detailVC.selectedImagePath = flagImagePaths[indexPath.row]
+            
+            navigationController?.pushViewController(detailVC, animated: true)
+        }
+    }
 }
 
