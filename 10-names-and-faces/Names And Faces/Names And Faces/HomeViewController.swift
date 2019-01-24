061e9f1ef3c3aab2fcfca817be8090a80d39b083
CypherPoet <CypherPoet@gmail.com>
2019-01-24 01:51:57 -0500

    initial setup for project 10

---
+++ 10-names-and-faces/Names And Faces/Names And Faces/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names And Faces
+//
+//  Created by Brian Sipple on 1/23/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
