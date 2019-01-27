7e449394ee32d77cac1996362c6219cd21c78746
CypherPoet <CypherPoet@gmail.com>
2019-01-27 02:23:37 -0500

    Add outlets to ViewController

--- 13-instafilter/Instafilter/Instafilter/ViewController.swift
+++ 13-instafilter/Instafilter/Instafilter/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensitySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
 
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
