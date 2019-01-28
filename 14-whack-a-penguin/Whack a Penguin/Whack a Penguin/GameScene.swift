94d109586a5b2f1c39cbcdb9b994734b1b2d5ff0
CypherPoet <CypherPoet@gmail.com>
2019-01-27 23:33:13 -0500

    Initialize project 14 and clean up sample code

---
+++ 14-whack-a-penguin/Whack a Penguin/Whack a Penguin/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  Whack a Penguin
+//
+//  Created by Brian Sipple on 1/27/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+}
