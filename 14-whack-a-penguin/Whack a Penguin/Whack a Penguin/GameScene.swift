894649278c84e821676b9726d85179012395d223
CypherPoet <CypherPoet@gmail.com>
2019-01-28 00:49:45 -0500

    Setup initial UI with background and score label

--- 14-whack-a-penguin/Whack a Penguin/Whack a Penguin/GameScene.swift
+++ 14-whack-a-penguin/Whack a Penguin/Whack a Penguin/GameScene.swift
@@ -7,15 +7,51 @@
 //
 
 import SpriteKit
-import GameplayKit
+
+let sceneWidth = 1024.0
+let sceneHeight = 768.0
 
 class GameScene: SKScene {
+    var currentScoreLabel: SKLabelNode!
     
-    override func didMove(to view: SKView) {
-        <#code#>
+    var currentScore = 0 {
+        didSet {
+            currentScoreLabel.text = "Score: \(self.currentScore)"
+        }
     }
     
+    
+    override func didMove(to view: SKView) {
+        setupBackground()
+        setupUI()
+        
+        currentScore = 0
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
+        
+    }
+    
+    
+    func setupBackground() {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        
+        background.position = CGPoint(x: sceneWidth / 2, y: sceneHeight / 2)
+        background.blendMode = .replace
+        background.zPosition = -1
+        
+        addChild(background)
+    }
+    
+    
+    func setupUI() {
+        currentScoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        
+        currentScoreLabel.position = CGPoint(x: sceneWidth * 0.007, y: sceneHeight * 0.007)
+        currentScoreLabel.horizontalAlignmentMode = .left
+        currentScoreLabel.fontSize = 48
+        
+        addChild(currentScoreLabel)
     }
 }
