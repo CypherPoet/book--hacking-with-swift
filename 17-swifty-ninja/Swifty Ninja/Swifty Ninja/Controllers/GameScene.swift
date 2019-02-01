29663565633dd679490b3f70fd4a6613a53a7339
CypherPoet <CypherPoet@gmail.com>
2019-02-01 00:26:49 -0500

    functionality for drawing slice trails

--- 17-swifty-ninja/Swifty Ninja/Swifty Ninja/Controllers/GameScene.swift
+++ 17-swifty-ninja/Swifty Ninja/Swifty Ninja/Controllers/GameScene.swift
@@ -13,13 +13,13 @@ let sceneHeight = 768.0
 
 class GameScene: SKScene {
     var scoreLabel: SKLabelNode!
-    var livesImageNodes = [SKSpriteNode]()
-    var activeSliceBG: SKShapeNode!
-    var activeSliceFG: SKShapeNode!
+    var sliceTrailBG: SKShapeNode!
+    var sliceTrailFG: SKShapeNode!
     
+    var livesImageNodes = [SKSpriteNode]()
+    var sliceTrailPoints = [CGPoint]()
     
     var remainingLives = 3
-    
     
     var currentScore = 0 {
         didSet {
@@ -31,13 +31,37 @@ class GameScene: SKScene {
         return CGPoint(x: sceneWidth / 2, y: sceneHeight / 2)
     }()
     
+    
     override func didMove(to view: SKView) {
         setupUI()
         setupPhysics()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        sliceTrailPoints.removeAll(keepingCapacity: true)
+        
+        guard let touch = touches.first else { return }
+        
+        beginDrawingSlices(afterTouch: touch)
+    }
+    
+    /**
+     *
+     */
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        sliceTrailPoints.append(touch.location(in: self))
+        redrawSliceTrails()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fadeOutSliceTrails()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touchesEnded(touches, with: event)
     }
     
     
@@ -45,7 +69,7 @@ class GameScene: SKScene {
         createBackground()
         createScoreLabel()
         createLivesImages()
-        createSlicesMarks()
+        createSlicesTrails()
         
         currentScore = 0
     }
@@ -90,21 +114,21 @@ class GameScene: SKScene {
      * Swiping around the screen will lead a glowing trail
      * of slice marks that fade away when the user lets go or keeps moving.
      */
-    func createSlicesMarks() {
-        activeSliceBG = SKShapeNode()
-        activeSliceFG = SKShapeNode()
+    func createSlicesTrails() {
+        sliceTrailBG = SKShapeNode()
+        sliceTrailFG = SKShapeNode()
         
-        activeSliceBG.zPosition = 2
-        activeSliceFG.zPosition = 2
+        sliceTrailBG.zPosition = 2
+        sliceTrailFG.zPosition = 2
         
-        activeSliceBG.strokeColor = UIColor(red: 1.0, green: 0.9, blue: 0, alpha: 1)
-        activeSliceFG.strokeColor = UIColor.white
+        sliceTrailBG.strokeColor = UIColor(red: 1.0, green: 0.9, blue: 0, alpha: 1)
+        sliceTrailFG.strokeColor = UIColor.white
         
-        activeSliceBG.lineWidth = 9
-        activeSliceFG.lineWidth = 5
+        sliceTrailBG.lineWidth = 9
+        sliceTrailFG.lineWidth = 5
         
-        addChild(activeSliceBG)
-        addChild(activeSliceFG)
+        addChild(sliceTrailBG)
+        addChild(sliceTrailFG)
     }
     
     
@@ -113,4 +137,56 @@ class GameScene: SKScene {
         physicsWorld.speed = 0.85
     }
     
+    
+    /**
+     * - If we have fewer than two points in our array, we don't have enough
+     *   data to draw a line so it needs to clear the shapes and exit the method
+     *
+     * - If we have more than 12 slice points in our array, we need to remove the
+     *   oldest ones until we have at most 12. This stops the swipe shapes from becoming too long
+     */
+    func redrawSliceTrails() {
+        guard sliceTrailPoints.count > 2 else {
+            sliceTrailBG.path = nil
+            sliceTrailFG.path = nil
+            return
+        }
+        
+        if (sliceTrailPoints.count > 12) {
+            sliceTrailPoints.removeFirst(sliceTrailPoints.count - 12)
+        }
+        
+        let path = UIBezierPath()
+
+        path.move(to: sliceTrailPoints.first!)
+        
+        for index in 1 ..< sliceTrailPoints.count {
+            path.addLine(to: sliceTrailPoints[index])
+        }
+        
+        sliceTrailBG.path = path.cgPath
+        sliceTrailFG.path = path.cgPath
+    }
+    
+    
+    func fadeOutSliceTrails() {
+        sliceTrailBG.run(SKAction.fadeOut(withDuration: 0.25))
+        sliceTrailFG.run(SKAction.fadeOut(withDuration: 0.25))
+    }
+    
+    
+    func beginDrawingSlices(afterTouch touch: UITouch) {
+        sliceTrailPoints.removeAll(keepingCapacity: true)
+        sliceTrailPoints.append(touch.location(in: self))
+        
+        redrawSliceTrails()
+        
+        // remove any actions that are currently associated with our slice trails
+        // and set their alpha to 1
+        sliceTrailBG.removeAllActions()
+        sliceTrailFG.removeAllActions()
+        
+        sliceTrailBG.alpha = 1
+        sliceTrailFG.alpha = 1
+    }
 }
