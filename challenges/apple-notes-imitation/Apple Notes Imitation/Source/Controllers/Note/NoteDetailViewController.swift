84b43646ecb409f257c094fa7475bc57050e5a4b
CypherPoet <CypherPoet@gmail.com>
2019-02-05 04:22:54 -0500

    First pass at attempting to handle keyboard changes in detail view

--- challenges/apple-notes-imitation/Apple Notes Imitation/Source/Controllers/Note/NoteDetailViewController.swift
+++ challenges/apple-notes-imitation/Apple Notes Imitation/Source/Controllers/Note/NoteDetailViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class NoteDetailViewController: UIViewController {
+    @IBOutlet weak var noteTextView: UITextView!
+    
     var note: Note!
+    let keyboardNotificationNames = [UIResponder.keyboardWillHideNotification, UIResponder.keyboardWillChangeFrameNotification]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,7 @@ class NoteDetailViewController: UIViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         setupNavbar()
+        setupNotificationObservers()
     }
     
     
@@ -37,6 +42,41 @@ class NoteDetailViewController: UIViewController {
             target: self,
             action: #selector(shareNote)
         )
+    }
+    
+    func setupNotificationObservers() {
+        let notificationCenter = NotificationCenter.default
+        
+        for notificationName in keyboardNotificationNames {
+            notificationCenter.addObserver(self, selector: #selector(adjustForKeyboardMovements), name: notificationName, object: nil)
+        }
+    }
+    
+    
+    @objc func adjustForKeyboardMovements(notification: NSNotification) {
+        // TODO: Figure out why this ends up sliding the text out of view to the side:
+        
+//        guard let userInfo = notification.userInfo else { return }
+//
+//        let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+//        let keybardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+//
+//        if notification.name == UIResponder.keyboardWillHideNotification {
+//            // 🔑 workaround for hardware keyboards being connected
+//            noteTextView.contentInset = UIEdgeInsets.zero
+//        } else {
+//            noteTextView.contentInset = UIEdgeInsets(
+//                top: 0,
+//                left: 0,
+//                bottom: keybardViewEndFrame.height,
+//                right: keybardViewEndFrame.width
+//            )
+//        }
+//
+//        noteTextView.scrollIndicatorInsets = noteTextView.contentInset
+//
+//        // scroll to the current positoin of the text entry cursor if it's off screen
+//        noteTextView.scrollRangeToVisible(noteTextView.selectedRange)
     }
     
     
