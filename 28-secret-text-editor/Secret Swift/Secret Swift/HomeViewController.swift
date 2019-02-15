b1f841105b8e27e42bf23898abeb5586861c48b6
CypherPoet <CypherPoet@gmail.com>
2019-02-15 07:39:14 -0500

    Finish first version of Project 28

--- 28-secret-text-editor/Secret Swift/Secret Swift/HomeViewController.swift
+++ 28-secret-text-editor/Secret Swift/Secret Swift/HomeViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import KeychainAccess
+import LocalAuthentication
+
+enum Title: String {
+    case nondescript = "✌️ Swift Notes"
+    case unlocked = "🔓 Secret Unlocked"
+}
+
+enum ViewMode: String {
+    case secretHidden
+    case secretShowing
+}
 
 class HomeViewController: UIViewController {
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var secretTextView: UITextView!
     
     lazy var notificationCenter = NotificationCenter.default
     
@@ -18,21 +30,51 @@ class HomeViewController: UIViewController {
         UIResponder.keyboardWillChangeFrameNotification,
     ]
     
+    lazy var keychain = Keychain(service: "io.sipple.Secret-Swift")
+    lazy var authContext = LAContext()
+    
+    var currentViewMode = ViewMode.secretHidden {
+        didSet {
+            viewModeChanged()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         addNotificationObservers()
     }
-
     
     func addNotificationObservers() {
         for notification in keyboardMovementNotifications {
             notificationCenter.addObserver(self, selector: #selector(handleKeyboardMovement), name: notification, object: nil)
         }
+        
+        notificationCenter.addObserver(
+            self, selector: #selector(saveSecretMessage), name: UIApplication.willResignActiveNotification, object: nil
+        )
     }
     
+    func unlockSecretMessage() {
+        currentViewMode = .secretShowing
+        
+        if let secretText = try? keychain.getString("secretText") {
+            secretTextView.text = secretText
+        }
+    }
+    
+    @objc func saveSecretMessage() {
+        guard currentViewMode == .secretShowing else { return }
+        
+        keychain[string: "secretText"] = secretTextView.text
+        
+        // have the text view give up focus so the keyboard will hide
+        secretTextView.resignFirstResponder()
+        currentViewMode = .secretHidden
+    }
 
+    
     @objc func handleKeyboardMovement(notification: Notification) {
         guard let userInfo = notification.userInfo else { return }
         
@@ -40,17 +82,66 @@ class HomeViewController: UIViewController {
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
         
         if notification.name == UIResponder.keyboardWillHideNotification {
-            textView.contentInset = UIEdgeInsets.zero
+            secretTextView.contentInset = UIEdgeInsets.zero
         } else {
-            textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+            secretTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
         }
         
-        textView.scrollIndicatorInsets = textView.contentInset
-        textView.scrollRangeToVisible(textView.selectedRange)
+        secretTextView.scrollIndicatorInsets = secretTextView.contentInset
+        secretTextView.scrollRangeToVisible(secretTextView.selectedRange)
+    }
+    
+    
+    func viewModeChanged() {
+        switch currentViewMode {
+        case .secretHidden:
+            title = Title.nondescript.rawValue
+            secretTextView.isHidden = true
+        case .secretShowing:
+            title = Title.unlocked.rawValue
+            secretTextView.isHidden = false
+        }
+    }
+    
+    
+    func authFailed() {
+        let alertController = UIAlertController(
+            title: "Authentication Failed",
+            message: "You could not be verified. Please try again",
+            preferredStyle: .alert
+        )
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(alertController, animated: true)
     }
     
     
     @IBAction func authenticateTapped(_ sender: Any) {
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            authContext.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: reason,
+                reply: { (success: Bool, error: Error?) in
+                    DispatchQueue.main.async { [unowned self] in
+                        if success {
+                            self.unlockSecretMessage()
+                        } else {
+                            self.authFailed()
+                        }
+                        
+                    }
+                }
+            )
+            
+        } else {
+            // 📝 TODO: Handle case where no biometrics are supported
+            print("No Bio")
+        }
     }
 }
 
