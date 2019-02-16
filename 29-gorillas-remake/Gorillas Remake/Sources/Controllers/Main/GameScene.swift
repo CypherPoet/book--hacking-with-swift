f1b0dea23abc6378b9e571eddf0bb93317aa5327
CypherPoet <CypherPoet@gmail.com>
2019-02-16 08:14:30 -0500

    initial functionality for aiming and throwing banana

--- 29-gorillas-remake/Gorillas Remake/Sources/Controllers/Main/GameScene.swift
+++ 29-gorillas-remake/Gorillas Remake/Sources/Controllers/Main/GameScene.swift
@@ -21,19 +21,38 @@ enum CollisionBitMasks: UInt32 {
 enum NodeNames: String {
     case banana
     case building
-    case player
+    case player1
+    case player2
 }
 
 let buildingSpacing = 2.0
+let buildingMinHeight = sceneHeight * 0.21
+let buildingMaxHeight = sceneHeight * 0.52
 
-let buildingMinHeight = sceneHeight * 0.30
-let buildingMaxHeight = sceneHeight * 0.54
+let throwSpeedMultipler = 0.10
 
 
 class GameScene: SKScene {
+    weak var gameViewController: GameViewController!
     lazy var sceneCenter = CGPoint(x: sceneWidth / 2.0, y: sceneHeight / 2.0)
     
     var buildings = [BuildingNode]()
+    lazy var banana = SKSpriteNode(imageNamed: "banana")
+    lazy var player1 = SKSpriteNode(imageNamed: "player")
+    lazy var player2 = SKSpriteNode(imageNamed: "player")
+    
+    lazy var player1ArmRaise = SKAction.setTexture(SKTexture(imageNamed: "player1Throw"))
+    lazy var player2ArmRaise = SKAction.setTexture(SKTexture(imageNamed: "player2Throw"))
+
+    var throwingAnimationSequence: SKAction {
+        let armRaiseAction = currentPlayer == .playerOne ? player1ArmRaise : player2ArmRaise
+        
+        return SKAction.sequence([
+            armRaiseAction,
+            SKAction.wait(forDuration: 0.15),
+            SKAction.setTexture(SKTexture(imageNamed: "player"))
+        ])
+    }
     
     var buildingHeight: Double {
         return Double.random(in: buildingMinHeight...buildingMaxHeight)
@@ -43,11 +62,33 @@ class GameScene: SKScene {
         return Double(40 * Int.random(in: 2...4))
     }
     
+    var currentPlayer: CurrentPlayer {
+        return gameViewController.currentPlayer
+    }
+    
+    var currentPlayerNode: SKSpriteNode {
+        return currentPlayer == .playerOne ? player1 : player2
+    }
+    
+    var throwXDirectionWeight: Double {
+        return currentPlayer == .playerOne ? 1.0 : -1.0
+    }
+    
+    var bananaStartingPosition: CGPoint {
+        switch currentPlayer {
+        case .playerOne:
+            return CGPoint(x: player1.position.x - 30.0, y: player1.position.y + 40.0)
+        case .playerTwo:
+            return CGPoint(x: player2.position.x + 30.0, y: player2.position.y + 40.0)
+        }
+    }
     
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(hue: 0.669, saturation: 0.99, brightness: 0.67, alpha: 1)
         
         createBuildings()
+        createPlayers()
+        createBanana()
     }
     
     
@@ -57,8 +98,6 @@ class GameScene: SKScene {
         while currentXPos < sceneWidth {
             let width = buildingWidth
             let height = buildingHeight
-            
-            print("Current X: \(currentXPos), Current width: \(width)")
             
             let building = BuildingNode(color: UIColor.red, size: CGSize(width: width, height: height))
             
@@ -70,5 +109,89 @@ class GameScene: SKScene {
             
             currentXPos += (width + buildingSpacing)
         }
+    }
+    
+    
+    /**
+     - Create player one and player two
+        - Player 1 is atop the second building
+        - Player 2 is atop the second-to-last building
+     - Create a non-dynamic physics body for the players that collides with bananas
+     */
+    func createPlayers() {
+        let leftBuilding = buildings[1]
+        let rightBuilding = buildings[buildings.count - 2]
+        
+        player1.name = NodeNames.player1.rawValue
+        player1.position = CGPoint(
+            x: leftBuilding.position.x,
+            y: leftBuilding.position.y + ((leftBuilding.size.height + player1.size.height) / 2)
+        )
+        
+        player2.name = NodeNames.player2.rawValue
+        player2.position = CGPoint(
+            x: rightBuilding.position.x,
+            y: rightBuilding.position.y + ((rightBuilding.size.height + player2.size.height) / 2)
+        )
+        
+        for playerNode in [player1, player2] {
+            playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
+            playerNode.physicsBody?.categoryBitMask = CollisionBitMasks.player.rawValue
+            playerNode.physicsBody?.collisionBitMask = CollisionBitMasks.banana.rawValue
+            playerNode.physicsBody?.contactTestBitMask = CollisionBitMasks.banana.rawValue
+            playerNode.physicsBody?.isDynamic = false
+            
+            addChild(playerNode)
+        }
+    }
+    
+    func createBanana() {
+        banana.name = NodeNames.banana.rawValue
+        banana.physicsBody = SKPhysicsBody(circleOfRadius: banana.size.height)
+        banana.physicsBody?.categoryBitMask = CollisionBitMasks.banana.rawValue
+        banana.physicsBody?.collisionBitMask = CollisionBitMasks.building.rawValue | CollisionBitMasks.player.rawValue
+        banana.physicsBody?.contactTestBitMask = CollisionBitMasks.building.rawValue | CollisionBitMasks.player.rawValue
+        banana.physicsBody?.usesPreciseCollisionDetection = true
+    }
+
+    /**
+     - Determine how hard and in what direction to throw the banana
+     - Remove any somehow-existing bananas
+     - Animate the current player throwing the banana and sent it in motion
+     */
+    func launch(angle: Int, speed: Int) {
+        currentPlayerNode.run(throwingAnimationSequence)
+        throwBanana(angle: angle, speed: speed)
+        
+        // on launch finished...
+        gameViewController.launchCompleted()
+        
+    }
+    
+    func throwBanana(angle degrees: Int, speed: Int) {
+        let radians = radiansFromDegrees(Double(degrees))
+        let speedMagnitude = Double(speed) * throwSpeedMultipler
+        
+        let xMomentum = cos(radians) * speedMagnitude * throwXDirectionWeight
+        let yMomentum = sin(radians) * speedMagnitude
+        
+        let velocity = CGVector(dx: xMomentum, dy: yMomentum)
+        
+        if self.intersects(banana) {
+            banana.removeFromParent()
+        }
+        
+        banana.position = bananaStartingPosition
+        banana.physicsBody?.angularVelocity = CGFloat(20) * CGFloat(throwXDirectionWeight)
+        
+        // actions
+        
+        addChild(banana)
+        banana.physicsBody?.applyImpulse(velocity)
+    }
+    
+    
+    func radiansFromDegrees(_ degrees: Double) -> Double {
+        return (degrees / Double.pi) * 180.0
     }
 }
--- 29-gorillas-remake/Gorillas Remake/Sources/Controllers/Main/GameViewController.swift
+++ 29-gorillas-remake/Gorillas Remake/Sources/Controllers/Main/GameViewController.swift
@@ -10,19 +10,73 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
 
+enum GameplayState {
+    case awaitingThrow
+    case throwing
+}
+
+enum CurrentPlayer {
+    case playerOne
+    case playerTwo
+}
+
+class GameViewController: UIViewController {
+    @IBOutlet weak var throwAngleSlider: UISlider!
+    @IBOutlet weak var throwAngleLabel: UILabel!
+    
+    @IBOutlet weak var throwVelocitySlider: UISlider!
+    @IBOutlet weak var throwVelocityLabel: UILabel!
+    
+    @IBOutlet weak var launchButton: UIButton!
+    @IBOutlet weak var playerIndicator: UILabel!
+    
+    var gameScene: GameScene!
+    
+    lazy var uiControls = [throwAngleSlider, throwAngleLabel, throwVelocityLabel, throwVelocitySlider, launchButton]
+    
+    var currentThrowAngle = 45 {
+        didSet {
+            throwAngleLabel.text = "Angle: \(currentThrowAngle)°"
+        }
+    }
+    
+    var currentThrowVelocity = 125 {
+        didSet {
+            throwVelocityLabel.text = "Velocity: \(currentThrowVelocity)"
+        }
+    }
+    
+    var currentPlayer = CurrentPlayer.playerOne {
+        didSet {
+            switch currentPlayer {
+            case .playerOne:
+                playerIndicator.text = "<<< PLAYER ONE"
+            case .playerTwo:
+                playerIndicator.text = "PLAYER TWO >>>"
+            }
+        }
+    }
+    
+    var gameplayState = GameplayState.awaitingThrow {
+        didSet {
+            gameplayStateChanged()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let gameScene = SKScene(fileNamed: "GameScene") as? GameScene {
                 // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+                self.gameScene = gameScene
+                gameScene.scaleMode = .aspectFill
                 
                 // Present the scene
-                view.presentScene(scene)
+                view.presentScene(gameScene)
+                gameScene.gameViewController = self
             }
             
             view.ignoresSiblingOrder = true
@@ -30,6 +84,13 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        
+        setupState()
+    }
+    
+    func setupState() {
+        angleChanged(throwAngleSlider)
+        velocityChanged(throwVelocitySlider)
     }
 
     override var shouldAutorotate: Bool {
@@ -46,5 +107,39 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    
+    func gameplayStateChanged() {
+        switch gameplayState {
+        case .throwing:
+            uiControls.forEach({ $0?.isHidden = true })
+        case .awaitingThrow:
+            uiControls.forEach({ $0?.isHidden = false })
+        }
+    }
+    
+    func launchCompleted() {
+        gameplayState = .awaitingThrow
+        
+        switch currentPlayer {
+        case .playerOne:
+            currentPlayer = .playerTwo
+        case .playerTwo:
+            currentPlayer = .playerOne
+        }
+    }
+    
+    @IBAction func angleChanged(_ sender: Any) {
+        currentThrowAngle = Int(throwAngleSlider.value)
+    }
+    
+    @IBAction func velocityChanged(_ sender: Any) {
+        currentThrowVelocity = Int(throwVelocitySlider.value)
+    }
+    
+    @IBAction func launchTapped(_ sender: Any) {
+        gameplayState = .throwing
+        gameScene.launch(angle: currentThrowAngle, speed: currentThrowVelocity)
     }
 }
