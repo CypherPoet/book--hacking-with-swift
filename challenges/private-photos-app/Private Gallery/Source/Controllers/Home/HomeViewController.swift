a0202dd7f59c008385e2891e410c91b59ab9beef
CypherPoet <CypherPoet@gmail.com>
2019-02-18 09:41:24 -0500

    Add NASA images and setup initial image loading flow

--- challenges/private-photos-app/Private Gallery/Source/Controllers/Home/HomeViewController.swift
+++ challenges/private-photos-app/Private Gallery/Source/Controllers/Home/HomeViewController.swift
@@ -7,15 +7,82 @@
 //
 
 import UIKit
+import KeychainAccess
+import LocalAuthentication
+
+
+enum ViewMode {
+    case publicFacing
+    case privatePhotos
+}
+
+enum KeychainAccessKey {
+    static let passphrase = "passphrase"
+}
+
+enum NavContent {
+    enum Title {
+        static let publicFacing = "🛰 Nasa Gallery"
+        static let privatePhotos = "👾 Welcome In"
+    }
+    enum viewModeToggle {
+        static let publicFacing = "🚀"
+        static let privatePhotos = "🙈"
+    }
+}
+
 
 class HomeViewController: UICollectionViewController {
     @IBOutlet weak var signInButton: UIBarButtonItem!
     
+    var photos = [UIImage]()
+    
+    var currentViewMode = ViewMode.publicFacing {
+        didSet {
+            viewModeChanged()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadImages()
     }
 
 
+    func viewModeChanged() {
+        switch currentViewMode {
+        case .publicFacing:
+            title = NavContent.Title.publicFacing
+        case .privatePhotos:
+            title = NavContent.Title.privatePhotos
+        }
+        
+        loadImages()
+    }
+    
+    
+    func loadImages() {
+        switch currentViewMode {
+        case .publicFacing:
+            loadPublicPhotos()
+        case .privatePhotos:
+            loadPrivatePhotos()
+        }
+    }
+    
+    func loadPrivatePhotos() {
+        
+    }
+    
+    func loadPublicPhotos() {
+        
+    }
+    
+    
+    @IBAction func addPhotoTapped(_ sender: Any) {
+    }
+    
 }
 
