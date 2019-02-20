42c0d46cec32af55bf296a0fbd9278b9a8997983
CypherPoet <CypherPoet@gmail.com>
2019-02-19 21:21:29 -0500

    initial setup for project 31

---
+++ 31-ipad-multitasking/Multibrowser/Multibrowser/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Brian Sipple on 2/19/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
