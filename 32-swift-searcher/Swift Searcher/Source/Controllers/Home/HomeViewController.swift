816a53d0b4ca79e36a33d3d620f538e58aca30d6
CypherPoet <CypherPoet@gmail.com>
2019-02-20 21:11:16 -0500

    JSON data loading for project models

--- 32-swift-searcher/Swift Searcher/Source/Controllers/Home/HomeViewController.swift
+++ 32-swift-searcher/Swift Searcher/Source/Controllers/Home/HomeViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-
+    var projects = [Project]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loadProjects()
     }
+    
 
-
+    func loadProjects() {
+        if let jsonData = loadProjectsJSON() {
+            let decoder = JSONDecoder()
+            
+            do {
+                projects = try decoder.decode([Project].self, from: jsonData)
+            } catch let error {
+                print("Error decoding projects JSON data:\n\(error.localizedDescription)")
+            }
+        } else {
+            print("Unable to find projects JSON data")
+        }
+    }
+    
+    func loadProjectsJSON() -> Data? {
+        if let dataPath = Bundle.main.path(forResource: "projects", ofType: "json") {
+            do {
+                let dataURL = URL(fileURLWithPath: dataPath)
+                return try Data(contentsOf: dataURL)
+            } catch let error {
+                print("Error while parsing projects json data: \(error.localizedDescription)")
+            }
+        }
+        
+        return nil
+    }
 }
 
---
+++ 32-swift-searcher/Swift Searcher/Source/Models/Project.swift
@@ -0,0 +1,12 @@
+//
+//  Project.swift
+//  Swift Searcher
+//
+//
+
+import Foundation
+
+struct Project: Codable {
+    var name: String
+    var subtitle: String
+}
