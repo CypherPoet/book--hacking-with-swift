ae2040cdaca19098637de4a875fb85b5cf3b2fe4
CypherPoet <CypherPoet@gmail.com>
2019-02-21 21:08:45 -0500

    initial setup of Project 33

---
+++ 32-swift-searcher/Swift Searcher/Source/Extensions/TableCellDisplayable.swift
@@ -0,0 +1,13 @@
+//
+//  TableCellDisplayable.swift
+//  Swift Searcher
+//
+//  Created by Brian Sipple on 2/20/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import Foundation
+
+protocol TableCellDisplayable {
+    func format()
+}
---
+++ 33-cloudkit-guess-the-song/Guess the Song/Source/Controllers/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Guess the Song
+//
+//  Created by Brian Sipple on 2/21/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func addSong(_ sender: Any) {
+    }
+}
+
