b87e0da3a7b512537705cf245966bc1c0c91bd83
CypherPoet <CypherPoet@gmail.com>
2019-02-26 00:53:12 -0500

    Initialize Project 34

---
+++ 34-gamekit-four-in-a-row/Four In A Row/Four In A Row/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Four In A Row
+//
+//  Created by Brian Sipple on 2/26/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
