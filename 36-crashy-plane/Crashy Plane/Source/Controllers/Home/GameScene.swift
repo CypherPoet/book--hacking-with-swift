878e7cba9261b7543fc67218b7162d2b8291e87c
CypherPoet <CypherPoet@gmail.com>
2019-03-03 00:26:34 -0500

    initialize Project 36

---
+++ 36-crashy-plane/Crashy Plane/Source/Controllers/Home/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  Crashy Plane
+//
+//  Created by Brian Sipple on 3/2/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    enum SceneParam {
+        static let width = 750.0
+        static let height = 1334.0
+    }
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
