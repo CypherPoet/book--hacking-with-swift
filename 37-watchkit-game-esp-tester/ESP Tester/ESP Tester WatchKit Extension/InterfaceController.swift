9f4d3e00cd64a4b1f76cd9d2f048ac652d252b23
CypherPoet <CypherPoet@gmail.com>
2019-03-08 04:57:09 -0500

    Add WatchKit app functionality for tapping wrist on `didReceiveMessage`

--- 37-watchkit-game-esp-tester/ESP Tester/ESP Tester WatchKit Extension/InterfaceController.swift
+++ 37-watchkit-game-esp-tester/ESP Tester/ESP Tester WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,25 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
 class InterfaceController: WKInterfaceController {
-
+    @IBOutlet var welcomeText: WKInterfaceLabel!
+    @IBOutlet var readyButton: WKInterfaceButton!
+    
+    lazy var watchConnectivitySession: WCSession? = {
+        guard WCSession.isSupported() else { return nil }
+        
+        let session = WCSession.default
+        session.delegate = self
+        
+        return session
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -21,11 +36,36 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        watchConnectivitySession?.activate()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    
+    // MARK: - Event handling
+    
+    @IBAction func readyButtonTapped() {
+        welcomeText.setHidden(true)
+        readyButton.setHidden(true)
+    }
+}
 
+
+// MARK: - WCSessionDelegate
+
+extension InterfaceController: WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        // TODO:
+    }
+    
+    /**
+     Whenever the watch recieves a message from the phone, it will tap the wearer's wrist
+     */
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        WKInterfaceDevice().play(.click)
+    }
 }
