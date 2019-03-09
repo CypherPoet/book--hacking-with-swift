1f4347e9740f0b69492a126421ed454f53ec1a88
CypherPoet <CypherPoet@gmail.com>
2019-03-09 00:35:18 -0500

    initialize Project 38

---
+++ 38-core-data-github-commits/Github Commits/Source/Controllers/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Commits
+//
+//  Created by Brian Sipple on 3/8/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
