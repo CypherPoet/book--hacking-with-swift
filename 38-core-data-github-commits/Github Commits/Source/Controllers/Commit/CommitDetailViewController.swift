ab9ca27e2d326953cb26bd7a6f4309b3e4c2106c
CypherPoet <CypherPoet@gmail.com>
2019-03-09 03:41:17 -0500

    Setup initial View Controller flow.

---
+++ 38-core-data-github-commits/Github Commits/Source/Controllers/Commit/CommitDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CommitDetailViewController.swift
+//  Github Commits
+//
+//  Created by Brian Sipple on 3/9/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class CommitDetailViewController: UIViewController {
+    @IBOutlet var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
